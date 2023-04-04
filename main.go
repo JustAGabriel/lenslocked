cf36828b5165b8fcfc0f2f355b4e4c6e68a251fb
Gabriel <45315755+JustAGabriel@users.noreply.github.com>
2023-04-04 07:37:31 +0200

    feat: add faq page routing

--- main.go
+++ main.go
@@ -15,12 +15,19 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page. Under construcion...</h1>")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	pageHTML := `<h1>FAQ Page. Under Construction...</h1>`
+	fmt.Fprint(w, pageHTML)
+}
+
 func (rtr Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/contact":
 		contactHandler(w, r)
 	case "/home":
 		homeHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page not found.", http.StatusNotFound)
 	}
