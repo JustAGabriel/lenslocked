e53b7cbff320f137943812600d35c6019443de80
Gabriel <45315755+JustAGabriel@users.noreply.github.com>
2023-04-18 07:35:49 +0200

    feat: add generic 'Must'

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/justagabriel/lenslocked/controllers"
+	"github.com/justagabriel/lenslocked/util"
 	"github.com/justagabriel/lenslocked/views"
 )
 
@@ -13,23 +14,17 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	tpl, err := views.GetTemplate("home")
-	if err != nil {
-		panic(err)
-	}
+	tpl := util.Must(views.GetTemplate("home"))
 	r.Get("/home", controllers.StaticHandler(tpl))
 
-	tpl, err = views.GetTemplate("contact")
-	if err != nil {
-		panic(err)
-	}
+	tpl = util.Must(views.GetTemplate("contact"))
 	r.Get("/contact", controllers.StaticHandler(tpl))
 
-	tpl, err = views.GetTemplate("faq")
-	if err != nil {
-		panic(err)
-	}
+	tpl = util.Must(views.GetTemplate("faq"))
 	r.Get("/faq", controllers.StaticHandler(tpl))
+
+	tpl = util.Must(views.GetTemplate("legal"))
+	r.Get("/legal", controllers.StaticHandler(tpl))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found, dude!", http.StatusNotFound)
