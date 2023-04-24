b6af4d62308e5bc4aa175af841da2591b2123ca8
Gabriel <45315755+JustAGabriel@users.noreply.github.com>
2023-04-24 21:23:09 +0200

    refactor: simplify templates

--- util/assert.go
+++ util/assert.go
@@ -1,5 +1,6 @@
 package util
 
+// Must asserts that a possible error return type is nil
 func Must[T any](t T, err error) T {
 	if err != nil {
 		panic(err)
--- views/template.go
+++ views/template.go
@@ -1,16 +1,10 @@
 package views
 
 import (
-	"errors"
 	"fmt"
 	"html/template"
 	"io/fs"
-	"log"
-	"net/http"
-	"os"
 	"path"
-
-	"github.com/justagabriel/lenslocked/util"
 )
 
 const (
@@ -18,22 +12,6 @@ const (
 	templateSubDirName = "templates"
 	templatesExtension = ".html"
 )
-
-func GetTemplate(filename string) (tmpl *template.Template, err error) {
-	rootPath, _ := os.Getwd()
-	filepath := path.Join(rootPath, viewsSubDirName, templateSubDirName, filename+templatesExtension)
-	if exists, err := util.FilepathExists(filepath); !exists {
-		errMsg := fmt.Sprintf("template path could not be found/accessed (%s): %v\n", filepath, err)
-		err = errors.New(errMsg)
-		return tmpl, err
-	}
-	tmpl, err = template.ParseFiles(filepath)
-	if err != nil {
-		errMsg := fmt.Sprintf("could not parse template (%s): %v\n", filepath, err)
-		return nil, errors.New(errMsg)
-	}
-	return tmpl, err
-}
 
 func ParseFS(fs fs.FS, templates ...string) (htmlTpl *template.Template, err error) {
 	var paths []string
@@ -48,25 +26,4 @@ func ParseFS(fs fs.FS, templates ...string) (htmlTpl *template.Template, err err
 		return
 	}
 	return htmlTpl, nil
-}
-
-func ExecuteTemplate(w http.ResponseWriter, filename string, data any) {
-	log.Println("filename: ", filename)
-	tmpl, err := GetTemplate(filename)
-	if err != nil {
-		log.Printf("could not retrieve template: %v\n", err)
-		http.Error(w, "Error while retrieving page.", http.StatusInternalServerError)
-		return
-	}
-	TryWriteTemplate(w, tmpl, data)
-}
-
-func TryWriteTemplate(w http.ResponseWriter, tmpl *template.Template, data any) {
-	err := tmpl.Execute(w, data)
-	if err != nil {
-		log.Printf("could not execute template: %v\n", err)
-		http.Error(w, "Error while retrieving page.", http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "text/html; charset=utf8")
 }
